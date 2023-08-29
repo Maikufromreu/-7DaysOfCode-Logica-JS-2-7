71740b499c4ff14198c45c3d20e0b140be7ef7f6
MaikuFromReu <synysterdark346@gmail.com>
2023-08-29 20:21:24 -0300

    finalizado caixa de perguntas e respostas

--- main.js
+++ main.js
@@ -1,10 +1,29 @@
 const botao = document.getElementById('btnResposta');
 const mensagem = document.getElementById('mensagem');
+const pergunta = document.getElementById("pergunta");
 
-const resposta = ['nome', 'idade', 'linguagem'];
+const resposta = {};
 
 botao.addEventListener ('click', () => {
-resposta['nome'] = mensagem.value;
-mensagem.value = '';
-})
+   if (pergunta.textContent === 'Como você se chama, humano??') {
+    resposta.nome = mensagem.value;
 
+   } else if (pergunta.textContent=== 'Quantos anos você tem?') {
+    resposta.idade = mensagem.value;
+
+   } else if (pergunta.textContent === 'Qual linguagem de programação você está estudando?') {
+    resposta.linguagem = mensagem.value;
+   }
+
+   mensagem.value = '';
+
+   if (pergunta.textContent === 'Como você se chama, humano??') {
+    pergunta.textContent = 'Quantos anos você tem?';
+
+   } else if (pergunta.textContent === 'Quantos anos você tem?') {
+    pergunta.textContent = 'Qual linguagem de programação você está estudando?';
+
+   } else {
+    console.log('Resposta:', resposta);
+   }
+})
